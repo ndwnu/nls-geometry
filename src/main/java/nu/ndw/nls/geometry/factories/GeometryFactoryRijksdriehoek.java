ec74f6545572caa709af1d202b3575aa0180c559
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2024-03-21 10:14:24 +0100

    #69181 WIP

--- src/main/java/nu/ndw/nls/geometry/factories/GeometryFactoryRijksdriehoek.java
+++ src/main/java/nu/ndw/nls/geometry/factories/GeometryFactoryRijksdriehoek.java
@@ -1,11 +1,13 @@
 package nu.ndw.nls.geometry.factories;
 
+
 import static nu.ndw.nls.geometry.constants.SRID.RIJKSDRIEHOEK;
 import static org.locationtech.jts.geom.PrecisionModel.FLOATING;
 
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class GeometryFactoryRijksdriehoek extends GeometryFactory {
