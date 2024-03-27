12eb264693b5067fefe3dbdc2c486073338ab236
Guido Senff <167181728+ndwlocatieservices@users.noreply.github.com>
2024-03-27 11:14:44 +0000

    Merged PR 46075: #69776 Fixing code coverage
    #69776 Fixing code coverage
    
    Related work items: #69776

--- src/main/java/nu/ndw/nls/geometry/factories/GeometryFactoryRijksdriehoek.java
+++ src/main/java/nu/ndw/nls/geometry/factories/GeometryFactoryRijksdriehoek.java
@@ -4,13 +4,18 @@ package nu.ndw.nls.geometry.factories;
 import static nu.ndw.nls.geometry.constants.SRID.RIJKSDRIEHOEK;
 import static org.locationtech.jts.geom.PrecisionModel.FLOATING;
 
+import java.io.Serial;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.stereotype.Component;
 
 
+
 @Component
 public class GeometryFactoryRijksdriehoek extends GeometryFactory {
+
+    @Serial
+    private static final long serialVersionUID = 1;
 
     public GeometryFactoryRijksdriehoek() {
         super(new PrecisionModel(FLOATING), RIJKSDRIEHOEK.value);
--- src/main/java/nu/ndw/nls/geometry/factories/GeometryFactoryWgs84.java
+++ src/main/java/nu/ndw/nls/geometry/factories/GeometryFactoryWgs84.java
@@ -4,12 +4,16 @@ package nu.ndw.nls.geometry.factories;
 import static nu.ndw.nls.geometry.constants.SRID.WGS84;
 import static org.locationtech.jts.geom.PrecisionModel.FLOATING;
 
+import java.io.Serial;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GeometryFactoryWgs84 extends GeometryFactory {
+
+    @Serial
+    private static final long serialVersionUID = 1;
 
     public GeometryFactoryWgs84() {
         super(new PrecisionModel(FLOATING), WGS84.value);
