bd185e6d5cfb10b30dbc2266e7d1c7d0ea7f4a99
dschimmel <167181728+ndwlocatieservices@users.noreply.github.com>
2024-04-12 15:40:30 +0200

    #70662 Moved geometry logic from alertc to this library

---
+++ src/main/java/nu/ndw/nls/geometry/mappers/PolylineDecoderMapper.java
@@ -0,0 +1,36 @@
+package nu.ndw.nls.geometry.mappers;
+
+import com.mapbox.geojson.utils.PolylineUtils;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import nu.ndw.nls.geometry.factories.GeometryFactoryWgs84;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.LineString;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class PolylineDecoderMapper {
+
+    private static final int DECODE_PRECISION_5_DECIMALS = 5;
+    private static final double SIMPLIFY_TOLERANCE_5_DECIMALS = Math.pow(10, -1.0 * DECODE_PRECISION_5_DECIMALS);
+    private static final boolean SIMPLIFY_HIGHEST_QUALITY = true;
+    private final GeometryFactoryWgs84 geometryFactoryWgs84;
+
+    public LineString map(Object geometry) {
+        if (geometry instanceof String) {
+            List<com.mapbox.geojson.Point> points =
+                    PolylineUtils.decode((String) geometry, DECODE_PRECISION_5_DECIMALS);
+            Coordinate[] coordinates =
+                    PolylineUtils.simplify(points, SIMPLIFY_TOLERANCE_5_DECIMALS, SIMPLIFY_HIGHEST_QUALITY)
+                            .stream()
+                            .map(p -> new Coordinate(p.longitude(), p.latitude()))
+                            .toArray(Coordinate[]::new);
+
+            return geometryFactoryWgs84.createLineString(coordinates);
+        }
+
+        throw new IllegalStateException("Geometry is not a valid encoded polyline: " + geometry);
+    }
+
+}
