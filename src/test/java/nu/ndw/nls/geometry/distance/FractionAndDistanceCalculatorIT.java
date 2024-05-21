d3ac526957cba76f3d231be684ab07532daa8560
Bas Duineveld <167181728+ndwlocatieservices@users.noreply.github.com>
2024-05-21 21:27:05 +0200

    #72500 Remove ExtractGeometryService. FractionAndDistanceCalculator is more precise

--- src/test/java/nu/ndw/nls/geometry/distance/FractionAndDistanceCalculatorIT.java
+++ src/test/java/nu/ndw/nls/geometry/distance/FractionAndDistanceCalculatorIT.java
@@ -1,14 +1,11 @@
 package nu.ndw.nls.geometry.distance;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import nu.ndw.nls.geometry.TestConfig;
 import nu.ndw.nls.geometry.distance.model.CoordinateAndBearing;
 import nu.ndw.nls.geometry.distance.model.FractionAndDistance;
 import nu.ndw.nls.geometry.factories.GeometryFactoryWgs84;
-import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.locationtech.jts.geom.Coordinate;
@@ -25,42 +22,44 @@ class FractionAndDistanceCalculatorIT {
     private static final Coordinate TO = new Coordinate(5.42672895, 52.17670980);
     private static final LineString LINE_STRING = new GeometryFactoryWgs84().createLineString(
             new Coordinate[]{FROM, TO});
-    private static final double DELTA = 0.00005;
+    private static final double FRACTION_DISTANCE_DELTA = 0.00005;
+    private static final double COORDINATE_DELTA = 0.0000001;
+    private static final double BEARING_DELTA = 0.5;
+
     @Autowired
     private FractionAndDistanceCalculator fractionAndDistanceCalculator;
-
 
     @Test
     void calculateFractionAndDistance_ok() {
         FractionAndDistance fractionAndDistance = fractionAndDistanceCalculator.calculateFractionAndDistance(
                 LINE_STRING, new Coordinate(5.426716016, 52.17672277));
-        assertEquals(0.4953, fractionAndDistance.getFraction(), DELTA);
-        assertEquals(1.6719, fractionAndDistance.getFractionDistance(), DELTA);
-        assertEquals(3.3758, fractionAndDistance.getTotalDistance(), DELTA);
+        assertEquals(0.4953, fractionAndDistance.getFraction(), FRACTION_DISTANCE_DELTA);
+        assertEquals(1.6719, fractionAndDistance.getFractionDistance(), FRACTION_DISTANCE_DELTA);
+        assertEquals(3.3758, fractionAndDistance.getTotalDistance(), FRACTION_DISTANCE_DELTA);
     }
 
     @Test
     void calculateFractionAndDistance_ok_beforeStartPointMinimum0() {
         FractionAndDistance fractionAndDistance = fractionAndDistanceCalculator.calculateFractionAndDistance(
                 LINE_STRING, new Coordinate(5.426702865, 52.176737201));
-        assertEquals(0.0, fractionAndDistance.getFraction(), DELTA);
-        assertEquals(0.0, fractionAndDistance.getFractionDistance(), DELTA);
-        assertEquals(3.3758, fractionAndDistance.getTotalDistance(), DELTA);
+        assertEquals(0.0, fractionAndDistance.getFraction(), FRACTION_DISTANCE_DELTA);
+        assertEquals(0.0, fractionAndDistance.getFractionDistance(), FRACTION_DISTANCE_DELTA);
+        assertEquals(3.3758, fractionAndDistance.getTotalDistance(), FRACTION_DISTANCE_DELTA);
     }
 
     @Test
     void calculateFractionAndDistance_ok_afterEndPointMaximum1() {
         FractionAndDistance fractionAndDistance = fractionAndDistanceCalculator.calculateFractionAndDistance(
                 LINE_STRING, new Coordinate(5.426729913, 52.17670903));
-        assertEquals(1.0, fractionAndDistance.getFraction(), DELTA);
-        assertEquals(3.3758, fractionAndDistance.getFractionDistance(), DELTA);
-        assertEquals(3.3758, fractionAndDistance.getTotalDistance(), DELTA);
+        assertEquals(1.0, fractionAndDistance.getFraction(), FRACTION_DISTANCE_DELTA);
+        assertEquals(3.3758, fractionAndDistance.getFractionDistance(), FRACTION_DISTANCE_DELTA);
+        assertEquals(3.3758, fractionAndDistance.getTotalDistance(), FRACTION_DISTANCE_DELTA);
     }
 
     @Test
     void calculateLengthInMeters_ok() {
         double lengthInMeters = fractionAndDistanceCalculator.calculateLengthInMeters(LINE_STRING);
-        assertEquals(3.3758, lengthInMeters, DELTA);
+        assertEquals(3.3758, lengthInMeters, FRACTION_DISTANCE_DELTA);
     }
 
     @Test
@@ -69,8 +68,11 @@ class FractionAndDistanceCalculatorIT {
         LineString subLineString = fractionAndDistanceCalculator.getSubLineString(originalLineString, 0.5);
         // Because we use the geodetic calculator, the answer is expected to slightly deviate from 53.005, which would
         // be the result of simple Cartesian interpolation.
-        assertEquals(createLineString(new Coordinate(5.0, 53.0), new Coordinate(5.0, 53.00500000211479)),
-                subLineString);
+        assertEquals(2, subLineString.getNumPoints());
+        assertEquals(5.0, subLineString.getCoordinateN(0).getX());
+        assertEquals(53.0, subLineString.getCoordinateN(0).getY());
+        assertEquals(5.0, subLineString.getCoordinateN(1).getX());
+        assertEquals(53.005, subLineString.getCoordinateN(1).getY(), COORDINATE_DELTA);
     }
 
     @Test
@@ -95,13 +97,25 @@ class FractionAndDistanceCalculatorIT {
         assertEquals(originalLineString, subLineString);
     }
 
+    @Test
+    void getSubLineString_ok_startEnd() {
+        LineString originalLineString = createLineString(new Coordinate(5.0, 53.0), new Coordinate(5.0, 53.01));
+        LineString subLineString = fractionAndDistanceCalculator.getSubLineString(originalLineString, 0.25, 0.75);
+        // Because we use the geodetic calculator, the answer is expected to slightly deviate from 53.0025 and 53.0075,
+        // which would be the result of simple Cartesian interpolation.
+        assertEquals(2, subLineString.getNumPoints());
+        assertEquals(5.0, subLineString.getCoordinateN(0).getX());
+        assertEquals(53.0025, subLineString.getCoordinateN(0).getY(), COORDINATE_DELTA);
+        assertEquals(5.0, subLineString.getCoordinateN(1).getX());
+        assertEquals(53.0075, subLineString.getCoordinateN(1).getY(), COORDINATE_DELTA);
+    }
 
     @Test
     void getCoordinateAndBearing_ok() {
         LineString originalLineString = createLineString(FROM, TO);
         CoordinateAndBearing coordinateAndBearing = fractionAndDistanceCalculator.getCoordinateAndBearing(
                 originalLineString, 0.5);
-        assertThat(coordinateAndBearing.bearing()).isCloseTo(149, Offset.offset(0.5));
+        assertEquals(149, coordinateAndBearing.bearing(), BEARING_DELTA);
     }
 
     private LineString createLineString(Coordinate... coordinates) {
