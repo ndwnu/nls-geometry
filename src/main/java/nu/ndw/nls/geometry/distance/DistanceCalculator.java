f30eee951aa02a567bfc1bc7ef1dd37c5d932ae3
NDW Locatie Services <167181728+ndwlocatieservices@users.noreply.github.com>
2025-02-04 14:36:10 +0100

    #83066 changed point to line distance calculation based on Rijksdriehoek transformation

--- src/main/java/nu/ndw/nls/geometry/distance/DistanceCalculator.java
+++ src/main/java/nu/ndw/nls/geometry/distance/DistanceCalculator.java
@@ -1,31 +1,45 @@
 package nu.ndw.nls.geometry.distance;
 
 import lombok.RequiredArgsConstructor;
-import nu.ndw.nls.geometry.factories.GeodeticCalculatorFactory;
-import nu.ndw.nls.geometry.factories.LocationIndexedLineFactory;
-import org.geotools.referencing.GeodeticCalculator;
-import org.locationtech.jts.geom.Coordinate;
+import nu.ndw.nls.geometry.constants.SRID;
+import nu.ndw.nls.geometry.crs.CrsTransformer;
+import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.LineString;
-import org.locationtech.jts.linearref.LinearLocation;
-import org.locationtech.jts.linearref.LocationIndexedLine;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.operation.distance.DistanceOp;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class DistanceCalculator {
 
-    private final LocationIndexedLineFactory locationIndexedLineFactory;
-    private final GeodeticCalculatorFactory geodeticCalculatorFactory;
+    private final CrsTransformer crsTransformer;
 
-    public double calculateDistance(Coordinate sourceCoordinate, LineString targetLineString) {
-        LocationIndexedLine locationIndexedLine = locationIndexedLineFactory.createLocationIndexedLine(targetLineString);
-        LinearLocation snappedLinearLocation = locationIndexedLine.project(sourceCoordinate);
-        Coordinate snappedPointCoordinate = snappedLinearLocation.getCoordinate(targetLineString);
+    /**
+     * Calculates the distance between a point and a line string. Both geometries have to be in WGS84 format.
+     *
+     * @param sourcePoint      the source point for the calculation
+     * @param targetLineString the target line string for the calculation
+     * @return the distance in meters
+     * @implNote Calculates the distance by first transforming the geometries to Rijksdriehoek. Because this CRS expresses points in meters,
+     * the JTS distance calculation will be more accurate than calculating the distance on the original geometries. A calculation by
+     * snapping the source point to the line string and then calculating the distance from the projected point to the line string with a
+     * geodetic calculator will introduce a noticeable error.
+     */
+    public double calculateDistance(Point sourcePoint, LineString targetLineString) {
+        validateWGS84(sourcePoint);
+        validateWGS84(targetLineString);
 
-        GeodeticCalculator geodeticCalculator = geodeticCalculatorFactory.createGeodeticCalculator();
-        geodeticCalculator.setStartingGeographicPoint(sourceCoordinate.getX(), sourceCoordinate.getY());
-        geodeticCalculator.setDestinationGeographicPoint(snappedPointCoordinate.getX(), snappedPointCoordinate.getY());
+        Point sourcePointRD = (Point) crsTransformer.transformFromWgs84ToRdNew(sourcePoint);
+        LineString targetLineStringRD = (LineString) crsTransformer.transformFromWgs84ToRdNew(targetLineString);
 
-        return geodeticCalculator.getOrthodromicDistance();
+        return DistanceOp.distance(sourcePointRD, targetLineStringRD);
+    }
+
+    private void validateWGS84(Geometry geometry) {
+        SRID srid = geometry.getSRID() == 0 ? SRID.WGS84 : SRID.fromValue(geometry.getSRID());
+        if (srid != SRID.WGS84) {
+            throw new IllegalArgumentException("SRID must be WGS84 and is %s".formatted(srid));
+        }
     }
 }
--- src/test/java/nu/ndw/nls/geometry/distance/DistanceCalculatorIT.java
+++ src/test/java/nu/ndw/nls/geometry/distance/DistanceCalculatorIT.java
@@ -1,16 +1,20 @@
 package nu.ndw.nls.geometry.distance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.stream.Stream;
 import nu.ndw.nls.geometry.GeometryConfiguration;
+import nu.ndw.nls.geometry.factories.GeometryFactoryRijksdriehoek;
 import nu.ndw.nls.geometry.factories.GeometryFactoryWgs84;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -25,18 +29,21 @@ class DistanceCalculatorIT {
     @Autowired
     private GeometryFactoryWgs84 geometryFactoryWgs84;
 
+    @Autowired
+    private GeometryFactoryRijksdriehoek geometryFactoryRijksdriehoek;
+
     public static Stream<Arguments> calculateDistance_okArgumentsProvider() {
         return Stream.of(
                 Arguments.of(4.904839, 52.366449, 0.0),
                 Arguments.of(4.907883, 52.361309, 0.0),
-                Arguments.of(4.908418, 52.363990, 148.95011124124488)
+                Arguments.of(4.908418, 52.363990, 135.92555008685923)
         );
     }
 
     @ParameterizedTest
     @MethodSource("calculateDistance_okArgumentsProvider")
     void calculateDistance_ok(double sourceX, double sourceY, double expectedDistance) {
-        Coordinate sourceCoordinate = new Coordinate(sourceX, sourceY);
+        Point sourcePoint = geometryFactoryWgs84.createPoint(new Coordinate(sourceX, sourceY));
 
         Coordinate[] coordinates = {
                 new Coordinate(4.904839, 52.366449),
@@ -44,7 +51,22 @@ class DistanceCalculatorIT {
         };
         LineString targetLineString = geometryFactoryWgs84.createLineString(coordinates);
 
-        double result = distanceCalculator.calculateDistance(sourceCoordinate, targetLineString);
+        double result = distanceCalculator.calculateDistance(sourcePoint, targetLineString);
         assertThat(result).isEqualTo(expectedDistance);
+    }
+
+    @Test
+    void calculateDistance_exception_notWgs84() {
+        Point sourcePoint = geometryFactoryRijksdriehoek.createPoint(new Coordinate(4.908418, 52.363990));
+
+        Coordinate[] coordinates = {
+                new Coordinate(4.904839, 52.366449),
+                new Coordinate(4.907883, 52.361309)
+        };
+        LineString targetLineString = geometryFactoryWgs84.createLineString(coordinates);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> distanceCalculator.calculateDistance(sourcePoint, targetLineString))
+                .withMessage("SRID must be WGS84 and is RIJKSDRIEHOEK");
     }
 }
