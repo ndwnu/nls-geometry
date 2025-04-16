44964d9480204d2805970732324ef6a8dacb87a2
Michel Durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2025-04-16 09:11:20 +0000

    Merged PR 82528: #92126 Add LineString merger collectors and tests
    #92126 Add LineString merger collectors and tests
    
    Introduce `LineStringMergerCollector` and `ToSingleLineStringMergerCollector` for merging `LineString` objects with the JTS library. Add `GeometryCollectors` utility class to simplify collector usage and implement corresponding unit tests to ensure functionality and correctness.
    
    Related work items: #92126

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,14 @@
 # Change Log
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [5.0.2] - 2025-04-15
+
+Restored `LineStringMergerCollector` and `ToSingleLineStringMergerCollector` for merging `LineString` objects with the JTS library.
+Restored `GeometryCollectors` utility class to simplify collector usage.
 
 ## [5.0.0] - 2025-04-04
 
@@ -53,14 +59,15 @@ If no coordinate reference system is provided, it assumes WGS84.
 ## [3.2.0] - 2024-10-31
 
 ### Added
-- Added mappers from GeoJson to Jts LineString (generic, wgs84 and rijksdriehoek) 
 
+- Added mappers from GeoJson to Jts LineString (generic, wgs84 and rijksdriehoek)
 
 ## [3.0.3] - 2024-09-06
 
 Here we write upgrade notes. It's a team effort to make them as straightforward as possible.
 
 ### Added
+
 - MINOR Added this changelog file
 
 ### Changed
---
+++ src/main/java/nu/ndw/nls/geometry/merging/LineStringMergerCollector.java
@@ -0,0 +1,53 @@
+package nu.ndw.nls.geometry.merging;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.operation.linemerge.LineMerger;
+
+/**
+ * Stateful collector that makes use of {@link LineMerger} to merge multiple {@link LineString}s. If all line strings can be combined into a
+ * single LineString then the result will be a list with a single result. If there are gaps in the line string, then multiple results will
+ * be returned.
+ */
+public class LineStringMergerCollector implements Collector<LineString, LineMerger, List<LineString>> {
+
+    @Override
+    public Supplier<LineMerger> supplier() {
+        return LineMerger::new;
+    }
+
+    @Override
+    public BiConsumer<LineMerger, LineString> accumulator() {
+        return LineMerger::add;
+    }
+
+    @Override
+    public BinaryOperator<LineMerger> combiner() {
+        return (lineMerger, lineMerger2) -> {
+            supplier().get().add(lineMerger.getMergedLineStrings());
+            supplier().get().add(lineMerger2.getMergedLineStrings());
+            return lineMerger;
+        };
+    }
+
+    @Override
+    public Function<LineMerger, List<LineString>> finisher() {
+        return lineMerger -> {
+            @SuppressWarnings("unchecked")
+            List<LineString> mergedLineStrings = (List<LineString>) lineMerger.getMergedLineStrings();
+            return mergedLineStrings;
+        };
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.emptySet();
+    }
+}
