04961e13b1c8e4214c4e1f57191b10728081084b
m durieux <167181728+ndwlocatieservices@users.noreply.github.com>
2025-12-16 16:46:54 +0100

    #102745 update MultiLineStringPointGeometryMatcher to use geodetic calculations for precise point-to-multilinestring matching

--- src/main/java/nu/ndw/nls/geometry/jts/matchers/MultiLineStringPointGeometryMatcher.java
+++ src/main/java/nu/ndw/nls/geometry/jts/matchers/MultiLineStringPointGeometryMatcher.java
@@ -2,10 +2,16 @@ package nu.ndw.nls.geometry.jts.matchers;
 
 import static org.locationtech.jts.geom.util.LineStringExtracter.getLines;
 
+import java.util.Comparator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import nu.ndw.nls.geometry.crs.CrsTransformer;
-import org.locationtech.jts.geom.Geometry;
+import nu.ndw.nls.geometry.distance.FractionAndDistanceCalculator;
+import nu.ndw.nls.geometry.distance.model.CoordinateAndBearing;
+import nu.ndw.nls.geometry.distance.model.FractionAndDistance;
+import nu.ndw.nls.geometry.factories.GeodeticCalculatorFactory;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.geotools.referencing.GeodeticCalculator;
 import org.locationtech.jts.geom.LineString;
 import org.locationtech.jts.geom.MultiLineString;
 import org.locationtech.jts.geom.Point;
@@ -15,35 +21,41 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class MultiLineStringPointGeometryMatcher {
 
-    private final CrsTransformer crsTransformer;
+    private final FractionAndDistanceCalculator fractionAndDistanceCalculator;
+
+    private final GeodeticCalculatorFactory geodeticCalculatorFactory;
 
     /**
-     * Matches a point to a line from a MultiLineString that falls within a specified buffer distance. Transforms the point and lines to an
-     * RD coordinate reference system to ensure buffer distances in metres. Then checks if the point is near any line within the buffer
-     * distance and returns the matching line transformed back to the original reference system.
+     * Matches a given point to the closest line in a MultiLineString within a specified buffer distance. If multiple candidates are found,
+     * the closest line is returned. Throws an exception if no matching line is found.
      *
-     * @param multiLineString the MultiLineString containing lines to search for a match
-     * @param point           the Point to be matched to a line
-     * @param bufferInMetres  the buffer distance in meters within which the point should match a line
-     * @return the matched LineString if the point falls within the buffer distance of a line
-     * @throws IllegalStateException if no matching line is found for the given point
+     * @param multiLineString the MultiLineString geometry that contains the lines to check against
+     * @param point           the Point geometry to be matched to the closest line
+     * @param bufferInMetres  the maximum distance in meters within which a match is acceptable
+     * @return the LineString from the MultiLineString that is closest to the point within the buffer distance
+     * @throws IllegalStateException if no matching line is found within the specified buffer distance
      */
     public LineString matchPointOnMultiLine(MultiLineString multiLineString, Point point, int bufferInMetres) {
-        Point pointRd = (Point) crsTransformer.transformFromWgs84ToRdNew(point);
         //noinspection unchecked
-        List<Geometry> lines = getLines(multiLineString);
-        return lines
-                .stream()
-                .map(crsTransformer::transformFromWgs84ToRdNew)
-                .filter(line -> isPointNearLineWithBuffer(line, pointRd, bufferInMetres))
-                .findFirst()
-                .map(crsTransformer::transformFromRdNewToWgs84)
-                .map(LineString.class::cast)
+        List<LineString> lines = getLines(multiLineString);
+        return lines.stream()
+                .map(line -> calculateDistanceToLine(point, line))
+                .filter(p -> p.getLeft() <= bufferInMetres)
+                .min(Comparator.comparingDouble(Pair::getLeft))
+                .map(Pair::getRight)
                 .orElseThrow(() -> new IllegalStateException("No matching line found for point %s".formatted(point)));
     }
 
-    private boolean isPointNearLineWithBuffer(Geometry line, Point pointRd, int bufferInMetres) {
-        return line.buffer(bufferInMetres)
-                .contains(pointRd);
+    private Pair<Double, LineString> calculateDistanceToLine(Point point, LineString line) {
+        FractionAndDistance fractionAndDistance = fractionAndDistanceCalculator.calculateFractionAndDistance(line,
+                point.getCoordinate());
+        CoordinateAndBearing coordinateAndBearing = fractionAndDistanceCalculator.getCoordinateAndBearing(line,
+                fractionAndDistance.getFraction());
+        GeodeticCalculator geodeticCalculator = geodeticCalculatorFactory.createGeodeticCalculator();
+        geodeticCalculator.setStartingGeographicPoint(point.getX(), point.getY());
+        geodeticCalculator.setDestinationGeographicPoint(coordinateAndBearing.coordinate().getX(),
+                coordinateAndBearing.coordinate().getY());
+        double distance = geodeticCalculator.getOrthodromicDistance();
+        return new ImmutablePair<>(distance, line);
     }
 }
